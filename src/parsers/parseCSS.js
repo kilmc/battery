fd72ddd351e3174cdc15c84fd0009e2e59ad4935
Kilian McMahon <kil@kilmc.com>
2018-08-22 17:54:41 -0400

    Add more parsing features

--- src/parsers/parseCSS.js
+++ src/parsers/parseCSS.js
@@ -1,10 +1,10 @@
-const parseStyleBlock = (string) => {
-  const [className, body] = string
-    .replace(/\s/g,'')
-    .match(/(.*)\{(.*)\}/)
+export const parseStyleBlock = (string) => {
+  const [selectors, body] = string
+    .match(/(.*)\{([^)]+)\}/)
     .splice(1);
 
   const parsedBody = body
+    .replace(/\s/g,'')
     .split(';')
     .filter(x => x !== '')
     .reduce((acc,declaration) => {
@@ -14,7 +14,122 @@ const parseStyleBlock = (string) => {
       return acc;
     },{});
 
-  return { [className]: parsedBody };
+  return { [selectors.trim()]: parsedBody };
 };
 
-export default parseStyleBlock;
+export const formatKeywordValue = (srcValue,keywordValuesConfig) => {
+  const { values, separator = '' } = keywordValuesConfig;
+  const value = Object
+    .entries(values)
+    .filter(([,value]) => value === srcValue)[0][0];
+
+  return `${separator}${value}`;
+};
+
+export const formatPluginValue = (srcValue,propConfig,config) => {
+  const { enablePlugin } = propConfig;
+  const { type, values } = config.plugins.filter(plugin => plugin.name === enablePlugin)[0];
+
+  if (type === 'pattern') {
+    return srcValue;
+  } else if (type === 'lookup') {
+    return Object.entries(values)
+      .filter(([,value]) => value.toLowerCase() === srcValue.toLowerCase())[0][0];
+  } else {
+    return undefined;
+  }
+};
+
+export const determineValueType = (srcValue,propConfig,config) => {
+  const { keywordValues, enablePlugin } = propConfig;
+
+  let isKeywordValue;
+  let isPluginValue;
+
+  if (keywordValues) {
+    isKeywordValue = Object
+      .entries(keywordValues.values)
+      .filter(([,value]) => value === srcValue)[0];
+  }
+
+  if (enablePlugin) {
+    const { valueRegexString } = config.plugins
+      .filter(plugin => plugin.name === enablePlugin);
+
+    isPluginValue = new RegExp(valueRegexString).test(srcValue);
+  }
+
+  if (isKeywordValue) {
+    return 'keyword';
+  } else if (isPluginValue) {
+    return 'plugin';
+  }
+  else {
+    return undefined;
+  }
+};
+
+export const determineValue = (srcValue,propConfig,config) => {
+  const valueType = determineValueType(srcValue,propConfig,config);
+
+  switch(valueType) {
+    case 'keyword':
+      return formatKeywordValue(srcValue,propConfig.keywordValues);
+    case 'plugin':
+      return formatPluginValue(srcValue,propConfig,config);
+    default:
+      return `[no value found for '${srcValue}']`;
+  }
+};
+
+export const declarationsToClassName = (declarations,config) => {
+  return Object.entries(declarations).reduce((acc,declaration) => {
+    const [srcProp,srcValue] = declaration;
+    const propConfig = config.props
+      .filter(({ prop }) => prop === srcProp)[0];
+
+    const value = determineValue(srcValue,propConfig,config);
+    const { propName, separator = ''} = propConfig;
+
+    return acc.concat(`${propName}${separator}${value}`);
+  },[]);
+};
+
+export const styleBlockToClassNames = (string,config) => {
+  const parsedBlock = parseStyleBlock(string);
+
+  return Object.keys(parsedBlock).reduce((acc,selector) => {
+    acc[selector] = declarationsToClassName(parsedBlock[selector],config);
+    return acc;
+  },{});
+};
+
+// Resolver structure
+const config = {
+  props: [{
+    prop: 'border',
+    propName: 'border',
+    keywordValues: {
+      values: { default: '0.1rem solid #282828' },
+      separator: '-'
+    },
+    conversionResolvers: {
+      default: ['1px solid color(grey-500)']
+    }
+  },{
+    prop: 'border-radius',
+    propName: 'border',
+    keywordValues: {
+      values: { rounded: '0.2rem' },
+      separator: '-'
+    },
+    conversionResolvers: {
+      rounded: [
+        '$tiny-border-radius',
+        '6px',
+        '$border-radius'
+      ]
+    }
+  }]
+};
+
--- tests/parsers/parseCSS.test.js
+++ tests/parsers/parseCSS.test.js
@@ -1,10 +1,67 @@
 /* eslint-env jest, node */
 
-import parseCSS from '../../src/parsers/parseCSS';
+import {
+  parseStyleBlock,
+  styleBlockToClassNames,
+  declarationsToClassName,
+  determineValue,
+  determineValueType,
+  formatKeywordValue,
+  formatPluginValue
+} from '../../src/parsers/parseCSS';
 
-describe('parseCSS', () => {
+const zIndexConfig = {
+  prop: 'z-index',
+  propName: 'z',
+  enablePlugin: 'integers'
+};
+
+const justifyContentConfig = {
+  prop: 'justify-content',
+  propName: 'justify',
+  keywordValues: {
+    values: { end: 'flex-end' },
+    separator: '-'
+  }
+};
+
+const displayConfig = {
+  prop: 'display',
+  propName: '',
+  keywordValues: { values: { flex: 'flex' } }
+};
+
+const backgroundColorConfig = {
+  prop: 'background-color',
+  propName: 'bg',
+  separator: '-',
+  enablePlugin: 'colors'
+};
+
+const colorConfig = {
+  prop: 'color',
+  propName: '',
+  enablePlugin: 'colors'
+};
+
+const integersPlugin = {
+  name: 'integers',
+  type: 'pattern',
+  valueRegexString: '\\d+|-\\d+'
+};
+
+const colorsPlugin = {
+  name: 'colors',
+  type: 'lookup',
+  values: {
+    'black': '#000000',
+    'white': '#FFFFFF'
+  }
+};
+
+describe('parseStyleBlock', () => {
   it('converts a CSS string to a classObject', () => {
-    const cssString = `
+    const testStyleBlock = `
 .project-card {
   display: block;
   border-radius: 0.2rem;
@@ -12,7 +69,7 @@ describe('parseCSS', () => {
   padding: 1.8rem;
 }
 `;
-    expect(parseCSS(cssString)).toEqual({
+    expect(parseStyleBlock(testStyleBlock)).toEqual({
       '.project-card': {
         display: 'block',
         'border-radius': '0.2rem',
@@ -20,5 +77,159 @@ describe('parseCSS', () => {
         padding: '1.8rem'
       }
     });
+  });
+
+  it('preserves whitespace in the selector string', () => {
+    const testStyleBlock = `
+#header ul li .link {
+  text-decoration: none;
+}`;
+    expect(parseStyleBlock(testStyleBlock)).toEqual({
+      '#header ul li .link': {
+        'text-decoration': 'none'
+      }
+    });
+  });
+});
+
+describe('determineValue', () => {
+  describe('determineValueType', () => {
+    it('determines the value type of a srcValue', () => {
+      const config = {
+        plugins: [integersPlugin],
+        props: [justifyContentConfig,zIndexConfig]
+      };
+
+      const keywordTestValue = 'flex-end';
+      const integerTestValue = '1';
+
+      expect.assertions(2);
+      expect(determineValueType(keywordTestValue,justifyContentConfig,config)).toEqual('keyword');
+      expect(determineValueType(integerTestValue,zIndexConfig,config)).toEqual('plugin');
+    });
+  });
+
+  describe('formatKeywordValue', () => {
+    it('converts a srcValue to a formatted keywordValue', () => {
+      const keywordValueConfig = {
+        values: { end: 'flex-end' },
+        separator: '-'
+      };
+
+      const testValue = 'flex-end';
+      expect(formatKeywordValue(testValue,keywordValueConfig)).toEqual('-end');
+    });
+  });
+
+  describe('formatPluginValue', () => {
+    describe('pattern type', () => {
+      it('converts a srcValue to a formatted pluginValue', () => {
+        const config = {
+          plugins: [integersPlugin],
+          props: [zIndexConfig]
+        };
+
+        const testValue = '1';
+        expect(formatPluginValue(testValue,zIndexConfig,config)).toEqual('1');
+      });
+    });
+
+    describe('lookup type', () => {
+      it('converts a srcValue to a formatted pluginValue', () => {
+        const config = {
+          plugins: [colorsPlugin],
+          props: [backgroundColorConfig]
+        };
+
+        expect(formatPluginValue('#000000',backgroundColorConfig,config)).toEqual('black');
+        expect(formatPluginValue('#FFFFFF',backgroundColorConfig,config)).toEqual('white');
+      });
+
+      it('handles case-insensitivity', () => {
+        const config = {
+          plugins: [colorsPlugin],
+          props: [backgroundColorConfig]
+        };
+
+        expect(formatPluginValue('#fFfFfF',backgroundColorConfig,config)).toEqual('white');
+        expect(formatPluginValue('#FFFFFF',backgroundColorConfig,config)).toEqual('white');
+        expect(formatPluginValue('#ffffff',backgroundColorConfig,config)).toEqual('white');
+      });
+    });
+  });
+
+
+  it('returns an error string if the value could not be matched', () => {
+    const borderConfig = {
+      prop: 'border',
+      propName: 'border',
+      keywordValues: {
+        values: { default: '0.1rem solid #282828' },
+        separator: '-'
+      }
+    };
+
+    const config = {
+      props: [borderConfig]
+    };
+
+    const testValue = '1px solid color(grey-500)';
+    expect(determineValue(testValue,borderConfig,config)).toEqual('[no value found for \'1px solid color(grey-500)\']');
+  });
+});
+
+describe('declarationsToClassName', () => {
+  it('converts a simple declaration to a className',() => {
+    const config = {
+      props: [justifyContentConfig]
+    };
+    const testDeclarations = { 'justify-content': 'flex-end' };
+    expect(declarationsToClassName(testDeclarations,config)).toEqual(['justify-end']);
+  });
+});
+
+describe('styleBlockToClassNames', () => {
+  it('converts a declaration to a class', () => {
+    const config = {
+      props: [{
+        ...displayConfig,
+        keywordValues: { values: { block: 'block' } }
+      }]
+    };
+    const testStyleBlock = `
+.project-header--show {
+  display: block;
+}
+`;
+    expect(styleBlockToClassNames(testStyleBlock,config))
+      .toEqual({ '.project-header--show': ['block'] });
+  });
+
+  it('converts multiple declarations to an array of classes', () => {
+    const config = {
+      props: [
+        displayConfig,
+        justifyContentConfig,
+        zIndexConfig,
+        backgroundColorConfig,
+        colorConfig
+      ],
+      plugins: [integersPlugin,colorsPlugin]
+    };
+    const testStyleBlock = `
+.project-info {
+  display: flex;
+  justify-content: flex-end;
+  z-index: 10;
+  background-color: #000000;
+  color: #FFFFFF;
+}
+`;
+    expect(styleBlockToClassNames(testStyleBlock,config))
+      .toEqual({
+        '.project-info': [
+          'flex','justify-end','z10','bg-black','white'
+        ]
+      });
   });
 });
