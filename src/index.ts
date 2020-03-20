c50c10b03e67c869add5b3f88c99a2c654900c6b
Kilian McMahon <kil@kilmc.com>
2020-03-20 16:30:36 -0400

    Add types for configs to index file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { generateCSS } from './generateCSS';
+export { BatteryConfig } from 'types/battery-config';
+export { PropertyConfig } from 'types/property-config';
+export { PluginConfig } from 'types/plugin-config';
 
 export default generateCSS;
