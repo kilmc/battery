891d31c7b6f2630eac37a0fcae94b0c1629f1010
Kilian McMahon <kil@kilmc.com>
2022-04-10 00:14:48 +0200

    Update package and export ModifierFn types

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export { BatteryConfig } from './types/battery-config';
 export { PropertyConfig } from './types/property-config';
 export { PluginConfig } from './types/plugin-config';
 export { BatteryPlugin } from './battery-plugin';
+export { ModifierFn } from './types/plugin-config';
 
 export default generateCSS;
